17f12bb9cd99854d2c258d550b212344fc0029be
Yulia Kotlyarova <yuliakykyshka@gmail.com>
2021-04-04 17:09:58 +0300

    feat: add sort and categories stile logic

---
+++ src/components/AddButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const AddButton = () =>  {
+    const onAddPosition = () => {
+    };
+
+    return (
+        <button onClick = {onAddPosition}> Добавить </button>
+    )
+}
